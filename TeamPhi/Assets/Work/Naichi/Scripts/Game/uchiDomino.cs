83caa83d1c13efcc76913efe59c497217be6d052
naichilab <naichilab@live.jp>
2015-01-24 15:48:55 +0900

    直線に並べるサンプルシーンの追加

---
+++ TeamPhi/Assets/Work/Naichi/Scripts/Game/uchiDomino.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uchiDomino : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.renderer.material.color = this.renderer.material.color.Offset (Random.Range (0, 360), 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
