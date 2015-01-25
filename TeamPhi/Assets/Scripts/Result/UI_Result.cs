1bef794df10e96d568f08b37f2c67dabb680ed99
ほじょ <takumioden@yahoo.co.jp>
2015-01-25 14:51:47 +0900

    Merge commit '7cfc172b7476bd3a454815bb44e0e44d866cef33' into hojo_new

---
+++ TeamPhi/Assets/Scripts/Result/UI_Result.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UI_Result : MonoBehaviour {
+
+	private static UI_Result instance;
+	public static UI_Result Instance {
+		get {
+			if (instance == null) {
+				instance = (UI_Result)FindObjectOfType (typeof(UI_Result));
+				
+				if (instance == null) {
+					Debug.LogError ("UI_Game is nothing.");
+				}
+			}
+			return instance;
+		}
+	}
+
+	public	UI_NormaInfo	uiNorma;
+	public	UI_Score		uiScore;
+	public	UI_Score		uiBonus;
+
+	public void SetNormaClearFromList(List<ItemDefinition.ItemKind> list)
+	{
+		uiNorma.SetNormaClearFromList (list);
+	}
+
+	public void SetNormaClear(ItemDefinition.ItemKind kind)
+	{
+		uiNorma.SetItemEnable (kind);
+	}
+
+	public void SetTotalScore(int score)
+	{
+		uiScore.SetScore (score);
+	}
+
+	public void SetBonus(int bonus)
+	{
+		uiBonus.SetScore (bonus);
+	}
+}
---
+++ TeamPhi/Assets/Work/Maruchu/DominoRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DominoRotate : MonoBehaviour {
+
+	private		Vector3			m_vecRotate	= Vector3.zero;
+
+	void	Awake() {
+		m_vecRotate	= transform.localRotation.eulerAngles;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		m_vecRotate.y				+= (Time.deltaTime *180f);
+		transform.localRotation		= Quaternion.Euler( m_vecRotate);
+	}
+}
