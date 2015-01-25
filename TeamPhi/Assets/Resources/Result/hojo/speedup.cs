8be45e7f05ec9ce8c82d91dcc166da5f84b9ce9d
naichilab <naichilab@live.jp>
2015-01-25 09:24:46 +0900

    アニメーション取り込み

---
+++ TeamPhi/Assets/Resources/Result/hojo/speedup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speedup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (Input.GetKey (KeyCode.UpArrow)) {
+						animation ["runUnitychan"].speed = 200;
+				} else if (Input.GetKey (KeyCode.UpArrow)) {
+						animation ["runUnitychan"].speed--;
+				}
+	}
+}
